427c19a55e6f83e5c4e0e379bda2539e1894e579
Lapetina <victor.lrs95@gmail.com>
2019-09-05 01:29:29 -0300

    config Django ecomm

---
+++ foo/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/lapetina/Documents/Workspace/ecomm_fast_solution/foo/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

412de7c381428322d546b9fe5a231ccde270857b
Lapetina <victor.lrs95@gmail.com>
2019-09-20 01:46:15 -0300

    config - update django for 2.2.4

---
+++ core/tests/tests_views.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+
+from __future__ import absolute_import, unicode_literals
+from django.test import TestCase, Client
+from django.urls import reverse
+import unittest
+
+
+class indexViewTestCase(TestCase):
+
+    #função para iniciar o teste
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse(urlconf='index')
+
+
+    #função para finalizar o teste
+    def tearDown(self):
+        pass
+
+    def test_status_code(self):
+        #response = self.client.get('/')
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_templeta_used(self):
+        #response = self.client.get('/')
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'index.html')
+
+if __name__ == '__main__':
+    unittest.main()

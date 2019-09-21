718e20f4490ad90fe9bdadb0657f2066143ec600
Lapetina <victor.lrs95@gmail.com>
2019-09-21 18:56:18 -0300

    test - Tests views.py

--- core/tests/tests_views.py
+++ core/tests/tests_views.py
@@ -5,26 +5,24 @@ from django.test import TestCase, Client
 from django.urls import reverse
 import unittest
 
+from core import views
 
 class indexViewTestCase(TestCase):
 
     #função para iniciar o teste
     def setUp(self):
         self.client = Client()
-        self.url = reverse(urlconf='index')
-
+        self.url = reverse('index')
 
     #função para finalizar o teste
     def tearDown(self):
         pass
 
     def test_status_code(self):
-        #response = self.client.get('/')
         response = self.client.get(self.url)
         self.assertEquals(response.status_code, 200)
 
     def test_templeta_used(self):
-        #response = self.client.get('/')
         response = self.client.get(self.url)
         self.assertTemplateUsed(response, 'index.html')
 
